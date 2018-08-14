db3aed504a77b54b3b88436764c3e7f541a3ad31
汪佳 <wangjia@zhuojianchina.com>
2018-08-14 20:16:52 +0800

    1

--- springmvc/src/test/java/com/testfan/mybatis/BaseTest.java
+++
@@ -1,9 +0,0 @@
-package com.testfan.mybatis;
-
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
-
-@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/spring-mybatis.xml" })
-public class BaseTest extends AbstractTestNGSpringContextTests{
-
-}
